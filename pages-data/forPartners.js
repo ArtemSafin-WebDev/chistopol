f7a88d7f531f8576ca3d5df1246b6ecf1f6accfd
Artem Safin <artemsafin.webdev@gmail.com>
2025-04-25 13:49:57 +0300

    Страница для партнеров

---
+++ pages-data/forPartners.js
@@ -0,0 +1,8 @@
+const forPartners = {
+  "/for-partners.html": {
+    title: "Для партнеров",
+    darkHeader: true,
+  },
+};
+
+export default forPartners;
--- pages.config.js
+++ pages.config.js
@@ -2,11 +2,13 @@ import home from "./pages-data/home";
 import about from "./pages-data/about";
 import team from "./pages-data/team";
 import career from "./pages-data/career";
+import forPartners from "./pages-data/forPartners";
 const pagesConfig = {
   ...home,
   ...about,
   ...team,
   ...career,
+  ...forPartners,
 };
 
 export default pagesConfig;
