7875892e269baab82d0cad7358af8c02bc411e95
Artem Safin <artemsafin.webdev@gmail.com>
2025-05-27 17:16:36 +0300

    Иллюстрации по странице карьеры

--- src/js/successStories.ts
+++ src/js/successStories.ts
@@ -1,5 +1,9 @@
 import Swiper from "swiper";
 import "swiper/css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function successStories() {
   const elements = Array.from(
@@ -7,6 +11,88 @@ export default function successStories() {
   );
 
   elements.forEach((element) => {
+    let mm = gsap.matchMedia();
+    mm.add(
+      "(min-width: 641px)",
+      () => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: element,
+            start: "top bottom-=20%",
+          },
+          onComplete: () => {
+            element.classList.add("animation-complete");
+          },
+        });
+        tl.from(".success-stories__heading", {
+          autoAlpha: 0,
+          duration: 0.7,
+          y: 30,
+        });
+        tl.from(
+          ".success-stories__more",
+          {
+            autoAlpha: 0,
+            duration: 0.7,
+            y: 30,
+          },
+          ">-=0.3"
+        );
+        tl.from(
+          ".success-stories__card",
+          {
+            autoAlpha: 0,
+            duration: 0.7,
+            stagger: 0.2,
+            y: 30,
+          },
+          ">-=0.3"
+        );
+      },
+      element
+    );
+    mm.add(
+      "(max-width: 640px)",
+      () => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: element,
+            start: "top bottom-=20%",
+          },
+          onComplete: () => {
+            element.classList.add("animation-complete");
+          },
+        });
+        tl.from(".success-stories__heading", {
+          autoAlpha: 0,
+          duration: 0.7,
+          y: 30,
+        });
+        tl.addLabel("beforeCards");
+
+        tl.from(
+          ".success-stories__card",
+          {
+            autoAlpha: 0,
+            duration: 0.7,
+            stagger: 0.2,
+            y: 30,
+          },
+          ">-=0.3"
+        );
+
+        tl.from(
+          ".success-stories__more",
+          {
+            autoAlpha: 0,
+            duration: 0.7,
+            delay: 0.3,
+          },
+          "beforeCards"
+        );
+      },
+      element
+    );
     const container = element.querySelector<HTMLElement>(".swiper");
     if (!container) return;
     new Swiper(container, {
